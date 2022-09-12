adddfc43702da131f78e40e6af9aef0ee006b5aa
TheresaNlt <83827851+TheresaNlt@users.noreply.github.com>
2022-09-12 16:22:03 -0400

    Add files via upload

---
+++ mp/paths.py
@@ -0,0 +1,17 @@
+# ------------------------------------------------------------------------------
+# Module where paths should be defined.
+# ------------------------------------------------------------------------------
+import os
+
+# Path where intermediate and final results are stored
+storage_path = '/local/scratch/nb_tn'
+storage_data_path = os.path.join(storage_path, 'data')
+
+# Original data paths. TODO: set necessary data paths.
+original_data_paths = {'batch1': '/local/scratch/nb_tn'}
+
+# Path that represents JIP data structure for training and inference
+JIP_dir = os.path.join(storage_path, 'JIP')
+
+# Login for Telegram Bot
+telegram_login = {'chat_id': 'TODO', 'token': 'TODO'}
