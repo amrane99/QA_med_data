a4f7cc631b8623e58ba2f532aab193eb8bd68959
Nikita Maurer <nikita.maurer2000@gmail.com>
2023-06-15 23:27:04 +0200

    Added improved splitting method for ADAC dataset

--- scripts/prep_cardiac_data.py
+++ scripts/prep_cardiac_data.py
@@ -137,8 +137,13 @@ def repair_nifty(in_file_path, out_dir_path=None, prefix=None):
 
 def adac_repair_nifties(in_dir_path, out_dir_path, prefix=None, copy_all=False):
     r'''
+        - in_dir_path:  Input directory path to the directory where files to be repaired are stored.
+        - out_dir_path: Output directory path to a directory where the repaired files will be stored.
+        - [prefix]:     Optional prefix for the filename of the repaired image (defalt: None)
+        - [copy_all]:   Optional parameter to enable that also non-nifti files are copied (default: False)
+        
         The files to be repaired should be .nii.gz files
-        copy_all copies also non-nifti files
+        
     '''
     print("Starting conversion...")
     
@@ -191,7 +196,7 @@ def adac_repair_nifties(in_dir_path, out_dir_path, prefix=None, copy_all=False):
     print("Reparation of nifties terminated.")
 
 
-def adac_split_4d(in_dir_path, out_dir_path=None, key='4d'):
+def split_4d(in_dir_path, out_dir_path=None, key='4d'):
     r'''
         - in_dir_path:      Directory path that specifies the entry point of application.
         - [out_dir_path]:   Optional path that specifies the output directory for the copied directory structure with the split frames
@@ -216,11 +221,6 @@ def adac_split_4d(in_dir_path, out_dir_path=None, key='4d'):
 
                 # Read the image and get the size of the fourth dimension (#frames)
                 image = sitk.ReadImage(file_path)
-                #reader = sitk.ImageFileReader()
-                #reader.SetFileName(file_path)
-                #reader.LoadPrivateTagsOn()
-                #reader.ReadImageInformation()
-
                 numb_frames = image.GetSize()[3]
 
                 # Construct the updated out_dir_path and create sub directory for the frames
@@ -245,6 +245,78 @@ def adac_split_4d(in_dir_path, out_dir_path=None, key='4d'):
                     sitk.WriteImage(frame, out_file_path)
 
                     print(f"Frame {i+1} saved as ", out_file_path)
+
+
+def adac_split_4d(in_dir_path, out_dir_path, copy_all=False):
+    r'''
+        - in_dir_path:      Directory path that specifies the entry point of application.
+        - out_dir_path:     Directory path that specifies the output directory for the images.
+        - [copy_all]:       Optional parameter that enables copying of frames and labels (default: False).
+
+        This method searches all 4D-images (recognized by substing '4d' in filenames) in the input directory, splits the image, 
+        and saves all the frames according to the 'Medical Decathlon' directory structure in the output directory. 
+        If copy_all is set True, additionally existing labels and frames in the dataset are copied into the output directory.
+        Notice: This method is adjusted to be applied to the ACDC dataset with its naming convention. Also, this method does not work with SimpleITK version 1.2.4.
+    '''
+    os.makedirs(os.path.join(out_dir_path, "images_Tr"), exist_ok=True)
+    os.makedirs(os.path.join(out_dir_path, "images_Ts"), exist_ok=True)
+    if copy_all:
+        os.makedirs(os.path.join(out_dir_path, "opt_labels_Tr"), exist_ok=True)
+        os.makedirs(os.path.join(out_dir_path, "opt_labels_Ts"), exist_ok=True)
+        os.makedirs(os.path.join(out_dir_path, "opt_frames_Tr"), exist_ok=True)
+        os.makedirs(os.path.join(out_dir_path, "opt_frames_Ts"), exist_ok=True)
+    
+    for dir_path, dir_names, file_names in os.walk(in_dir_path):
+        for file_name in file_names:
+            
+            if '4d' in file_name:
+                file_path = os.path.join(dir_path, file_name)
+                patient_digits = file_name.split('_')[0][7:]
+                patient_nr = int(patient_digits)
+
+                image = sitk.ReadImage(file_path)
+                numb_frames = image.GetSize()[3]
+
+                if patient_nr < 100:
+                    dest_dir_name = "images_Tr"
+                else:
+                    dest_dir_name = "images_Ts"
+
+                print(f"\nSplitting image {patient_nr}:")
+                
+                for i in range(numb_frames):
+                    try:
+                        frame = image[:, :, :, i]
+                    except Exception as e:
+                        print(f"Please check the SimpleITK package version. The following error might be due to an outdated version (approved in v2.2.1).\n {e}")
+                    
+                    frame_digits = "%02d" % (i+1)
+                    out_file_name = f"{patient_digits}_{frame_digits}.nii.gz"
+                        
+                    out_file_path = os.path.join(out_dir_path, dest_dir_name, out_file_name)
+                    sitk.WriteImage(frame, out_file_path)
+                    print(f"Saved frame {i+1}/{numb_frames}")
+
+            if copy_all:
+                if ('gt' in file_name) | ('frame' in file_name):
+                    file_path = os.path.join(dir_path, file_name)
+                    file_name_split = file_name.split('_')
+                    patient_digits = file_name_split[0][7:]
+                    frame_digits = file_name_split[1][5:]
+                    patient_nr = int(patient_digits)
+                    
+                    if ('gt' in file_name) & (patient_nr < 100):
+                        dest_dir_name = "opt_labels_Tr"
+                    elif ('gt' in file_name) & (patient_nr > 100):
+                        dest_dir_name = "opt_labels_Ts"
+                    elif ('frame' in file_name) & (patient_nr < 100):
+                        dest_dir_name = "opt_frames_Tr"
+                    elif ('frame' in file_name) & (patient_nr > 100):
+                        dest_dir_name = "opt_frames_Ts"
+
+                    out_file_name = f"{patient_digits}_{frame_digits}.nii.gz"
+                    out_file_path = os.path.join(out_dir_path, dest_dir_name, out_file_name)
+                    shutil.copy(file_path, out_file_path)
 
 
 def dcm_to_nii(input_dir, output_dir):
